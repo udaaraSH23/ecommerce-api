baf1774cd67739879562f2e176652da3661eec27
udaaraSH23 <udarasenarath875@gmail.com>
2025-10-26 15:09:05 +0530

    Edited Server port

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ sequelize.sync({ alter: true }).then(() => {
 
 // start server
 const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
